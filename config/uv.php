bf032486bf3c17fda48059235d66c9d26df4f8f5
HoangNH <hoangnh@evolableasia.vn>
2017-06-13 14:43:46 +0700

    update

--- config/uv.php
+++ config/uv.php
@@ -10,7 +10,7 @@ return [
     'num_alert' => 10, // number rows for alert on top menu
     'upload_path' => public_path() . '/uploads/', // media_upload_path   
 	'upload_thumbs_path' => public_path() . '/uploads/thumbs/', // media_upload_path    
-    'upload_url' => $url . '/public/uploads/', // image path,
+    'upload_url' => $url . '/uploads/', // image path,
     'max_size_upload' => 8000000    
 ];
 ?>
