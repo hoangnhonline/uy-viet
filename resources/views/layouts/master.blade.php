01b1e5169ed6beb9c90faf9d42bb9a6c7f9b07e6
HoangNH <hoangnh@evolableasia.vn>
2017-06-19 14:27:06 +0700

    update

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -10,6 +10,7 @@
 
 <head>
 	<meta charset="UTF-8">
+	<META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta name="viewport" content="width=device-width, initial-scale=1">	
     <meta name="csrf-token" content="{{ csrf_token() }}">
