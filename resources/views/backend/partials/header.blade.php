ea303472bebc35f9d5f4950ec005855ab92ed908
HoangNH <hoangnh@evolableasia.vn>
2017-08-23 13:43:36 +0700

    .

--- resources/views/backend/partials/header.blade.php
+++ resources/views/backend/partials/header.blade.php
@@ -1,6 +1,6 @@
 <header class="main-header">
   <!-- Logo -->
-  <a href="{{ route('dashboard.index')}}" class="logo">
+  <a href="{{ route('shop.index')}}" class="logo">
     <!-- mini logo for sidebar mini 50x50 pixels -->
     <span class="logo-mini"><b>Uy Việt</b></span>
     <!-- logo for regular state and mobile devices -->
